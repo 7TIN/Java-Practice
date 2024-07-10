8d2d5cb81e99ed31e8568de24552d67dec0a1f70
7TIN <92209180+7TIN@users.noreply.github.com>
2024-07-10 13:34:24 +0000

    Multi

--- Inheritance/MultiInheritance.java
+++ Inheritance/MultiInheritance.java
@@ -2,9 +2,25 @@ package Inheritance;
 
 class A{
 
-    int age;
+    void Add(int a , int b){
+        System.out.println(a+b); 
+    }
+}
+class B extends A{
+    void Sub(int a, int b){
+        System.out.println(a-b);
+    }
 }
 
-public class MultiInheritance {
-    
+public class MultiInheritance extends B {
+    void multi(int a, int b){
+        System.out.println(a*b);
+    }
+
+    public static void main(String[] args) {
+        MultiInheritance m = new MultiInheritance();
+        m.Add(10, 20);
+        m.Sub(20, 10);
+        m.multi(10, 20);
+    }
 }
